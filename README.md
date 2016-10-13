8926ad51611a351f87074606b34c47025ea2422f
aaeviru <aaeviru@gmail.com>
2016-10-13 13:52:34 +0000

    doc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tools
+---------------------------------------------------
+Query Creation    
+init-topics.py:extract claims from patent  
+chasen-topic.py:get chasen results of claims 
+topic-query.py:Creat query from chasen results of claims 
+---------------------------------------------------
+
+
--- init-topics.py
+++ init-topics.py
@@ -1,15 +1,21 @@
 import os
 import os.path
 import re
-for root, dirs, files in os.walk("/home/ec2-user/data/topics"):
-	for name in files:
-		filename = root + '/' + name
-		if (filename[len(filename)-3] == 't'):
-			continue
-		fin = open(filename,"r")
-		fout = open(filename+".txt","w")
-		temp = re.findall(r"(?<=<CLAIM>).*(?=</CLAIM>)",fin.read(),re.DOTALL)[0]
-		temp = re.sub(r'<.*?>','',temp)
-		fout.write(temp)
-		fin.close()
-		fout.close()
+import sys
+
+if len(sys.argv) != 2:
+    print "input:topic-folder"
+    sys.exit(1)
+
+for root, dirs, files in os.walk(sys.argv[1]):
+    for name in files:
+        filename = root + '/' + name
+        if (filename[len(filename)-3] == 't'):
+                continue
+        fin = open(filename,"r")
+        fout = open(filename+".txt","w")
+        temp = re.findall(r"(?<=<CLAIM>).*(?=</CLAIM>)",fin.read(),re.DOTALL)[0]
+        temp = re.sub(r'<.*?>','',temp)
+        fout.write(temp)
+        fin.close()
+        fout.close()
