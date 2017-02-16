40f3e6e7652ec0b18040c4553ad7307592dfefaa
aaeviru <aaeviru@gmail.com>
2017-02-16 12:25:42 +0900

    02/16

--- jpTOutf.py
+++ jpTOutf.py
@@ -8,21 +8,21 @@ for i in range(1993,2003):
 	print num
 	for root, dirs, files in os.walk(sys.argv[1]+tmp):
 		for name in files:
-                	filename = root + '/' + name
-                	if filename[len(filename)-3] == 'T':
+           	filename = root + '/' + name
+           	if filename[len(filename)-3] == 'T':
 				num += 1
 				filename = filename[0:len(filename)-3]
-                		os.system("nkf -w -O %s %s" % (filename+"TXT", filename+"txt") )	
+               	os.system("nkf -w -O %s %s" % (filename+"TXT", filename+"txt") )	
 
 for i in range(1993,2003):
-        tmp = str(i)+'B'
-        print tmp
+	tmp = str(i)+'B'
+	print tmp
 	print num
-        for root, dirs, files in os.walk(sys.argv[1]+tmp):
-                for name in files:
-                        filename = root + '/' + name
-                        if filename[len(filename)-3] == 'T':
+	for root, dirs, files in os.walk(sys.argv[1]+tmp):
+		for name in files:
+			filename = root + '/' + name
+			if filename[len(filename)-3] == 'T':
 				num +=1
-                                filename = filename[0:len(filename)-3]
-                                os.system("nkf -w -O %s %s" % (filename+"TXT", filename+"txt") )  
+				filename = filename[0:len(filename)-3]
+				os.system("nkf -w -O %s %s" % (filename+"TXT", filename+"txt") )  
 print num
